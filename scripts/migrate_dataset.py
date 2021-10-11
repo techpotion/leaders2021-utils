3f7df081a168a39128ac64185b115f5be3cbeb09
Nikolay <varlamow.col@yahoo.com>
2021-10-11 13:20:51 +0300

    dataset update

--- scripts/migrate_dataset.py
+++ scripts/migrate_dataset.py
@@ -12,10 +12,12 @@ def read_dataset() -> pd.DataFrame:
     '''
     df: pd.DataFrame = pd.read_excel('assets/dataset.xlsx', 'Со спортзонами и видами спорта')
 
-    df = df.drop(columns=['Доступность.1']) # wtf??
+    df = df.drop(columns=['Доступность.1', 'Unnamed: 12',  'Unnamed: 13', 'Unnamed: 14'])
     df = df.rename({
         'id Объекта': 'object_id',
         'Объект': 'object_name',
+        'Широта (Latitude)': 'object_point_lat',
+        'Долгота (Longitude)': 'object_point_lng',
         'id Ведомственной Организации': 'departmental_organization_id',
         'Ведомственная Организация': 'departmental_organization_name',
         'id Спортзоны': 'sports_area_id',
