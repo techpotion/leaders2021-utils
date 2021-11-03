437274f3abb0860bb495ce589d5478347cc7677c
Nikolay <varlamow.col@yahoo.com>
2021-11-03 12:04:48 +0300

    added subways data

---
+++ scripts/subways.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import json
+from scripts.db_connection import connect_db
+
+def load_df() -> pd.DataFrame:
+    with open('assets/metro.geojson', encoding='utf-8') as f:
+        gj = json.load(f)
+    df = pd.DataFrame(columns=['name', 'name_en', 'line_colour', 'point_lat', 'point_lng'])
+    for row in gj['features']:
+        props = row['properties']
+        geom = row['geometry']
+        df = df.append({
+            'name': props['name'],
+            'name_en': props['name_en'],
+            'line_colour': props['colour'],
+            'point_lat': geom['coordinates'][1],
+            'point_lng': geom['coordinates'][0],
+        }, ignore_index=True)
+    return df
+
+def df_to_db(df: pd.DataFrame):
+    df = load_df()
+    engine = connect_db()
+    df.to_sql('subway_stations', engine, index=False, if_exists='replace')
+    engine.execute('''
+        ALTER TABLE "subway_stations"
+        ADD COLUMN "position" geometry;
+
+        UPDATE "subway_stations"
+        SET "position" = ST_SetSRID(ST_Point(point_lng, point_lat), 4326)
+    ''')
+
+def main():
+    df = load_df()
+    df_to_db(df)
+
+if __name__ == "__main__":
+    main()
